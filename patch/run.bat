2b4ae0d899a514b256e050add0472ca2ab23b476
Phongsakorn Wisetthon <boyphongsakorn@users.noreply.github.com>
2017-06-04 14:25:04 +0700

    Update run.bat

--- patch/run.bat
+++ patch/run.bat
@@ -1,3 +1,5 @@
+DEL run.bat
+wget http://boyphongsakorn.github.io/Optifine_Auto_Updater/patch/run.bat
 DEL MinecraftAuto.exe
 wget http://boyphongsakorn.github.io/Optifine_Auto_Updater/patch/MinecraftAuto.exe
 start "Minecraft" "MinecraftAuto.exe"
